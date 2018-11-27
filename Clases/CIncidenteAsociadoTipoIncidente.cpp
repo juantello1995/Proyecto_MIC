5a059a62d9a8399667898c36eb9d90852c60a038
sanchez-san <45395265+sanchez-san@users.noreply.github.com>
2018-11-27 16:39:16 +0100

    Se han añadido las clases al github
    Se han añadido las clases al github

---
+++ Clases/CIncidenteAsociadoTipoIncidente.cpp
@@ -0,0 +1,36 @@
+#include "CIncidenteAsociadoTipoIncidente.h"
+#include <time.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "string.h"
+
+using namespace std;
+
+CIncidenteAsociadoTipoIncidente::CIncidenteAsociadoTipoIncidente()
+{
+	int m_i_ID_Incidentes = 0;
+	int m_i_ID_Tipo_Incidente = 0;
+}
+
+
+CIncidenteAsociadoTipoIncidente::~CIncidenteAsociadoTipoIncidente()
+{
+}
+
+CIncidenteAsociadoTipoIncidente::CIncidenteAsociadoTipoIncidente(int ID_Incidentes, int ID_Tipo_Incidente)
+{
+	m_i_ID_Incidentes = ID_Incidentes;
+	m_i_ID_Tipo_Incidente = ID_Tipo_Incidente;
+}
+
+int CIncidenteAsociadoTipoIncidente:: getID_Incidente()
+{
+	return m_i_ID_Incidentes;
+}
+
+int CIncidenteAsociadoTipoIncidente:: getID_Tipo_Incidente()
+{
+	return m_i_ID_Tipo_Incidente;
+}
---
+++ Clases/CVehiculoAtiendeIncidente.cpp
@@ -0,0 +1,37 @@
+#include "CVehiculoAtiendeIncidente.h"
+#include <time.h>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "string.h"
+
+using namespace std;
+
+CVehiculoAtiendeIncidente::CVehiculoAtiendeIncidente()
+{
+	int m_i_matricula = 0;
+	int m_i_ID_incidente = 0;
+}
+
+CVehiculoAtiendeIncidente::CVehiculoAtiendeIncidente(int matricula, int incidente)
+{
+	m_i_matricula = matricula;
+	m_i_ID_incidente = incidente;
+}
+
+
+CVehiculoAtiendeIncidente::~CVehiculoAtiendeIncidente()
+{
+}
+
+int CVehiculoAtiendeIncidente:: getMatricula()
+{
+	return m_i_matricula;
+}
+
+int CVehiculoAtiendeIncidente:: getID_Incidente()
+{
+	return m_i_ID_incidente;
+}
